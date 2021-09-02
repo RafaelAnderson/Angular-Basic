52a25f148e13cc8cc8b1a9f627fd684a21b3b23e
RafaelAnderson <rp_9966@hotmail.com>
2021-09-02 00:46:18 -0500

    User Component

--- mi-web-app/src/app/app.module.ts
+++ mi-web-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+// app.component viene del app.component.ts
 import { AppComponent } from './app.component';
+// Componente Usuario
+import { UsuarioComponent } from './usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
