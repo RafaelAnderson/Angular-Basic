8b5900c4f1dd7da38e7b999f04ddd5be1c9cc5bc
RafaelAnderson <rp_9966@hotmail.com>
2021-09-03 22:50:03 -0500

    Estructura dinámicas

--- mi-web-app/src/app/usuario.component.ts
+++ mi-web-app/src/app/usuario.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
 })
 
 export class UsuarioComponent{
+  // Variable tipo array
+  usuarios = ['Rafael', 'Anderson', 'Lina'];
 
   // Variable
-  usuarioNombre = "Rafael Anderson";
+  usuarioNombre = "";
+
+  // Método ngIf
+  visible = false
+
+  constructor(){
+    // Controlar el tiempo de ejecución en JavaScript ( ngIf )
+    setTimeout( () => {
+      this.visible = true;
+    }, 3000);
+  }
+
+  // Método para agregar dinámicamente
+  onAgregarUsuario(){
+    this.usuarios.push(this.usuarioNombre);
+  }
 }
